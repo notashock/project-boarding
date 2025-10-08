74ec9c7899ff91210c03253439332d182f05d80f
Ashok Babu Bavireddy <130152937+notashock@users.noreply.github.com>
2025-10-08 16:24:55 +0530

    feat:add the strategy will be decided by the user

--- server/controllers/boardingController.js
+++ server/controllers/boardingController.js
@@ -6,22 +6,36 @@ const extractSeatNumber = (seat) => {
 };
 
 // GET /api/boarding-sequence
+// Optional query: ?strategy=max or ?strategy=min
 export const getBoardingSequence = async (req, res) => {
   try {
+    const { strategy = "max" } = req.query; // defaults to min if not provided
+
     const bookings = await Booking.find();
 
     if (!bookings || bookings.length === 0) {
       return res.status(404).json({ message: "No bookings found" });
     }
 
+    // calculate row number (minRow or maxRow depending on strategy)
     const processed = bookings.map((b) => {
-      const maxRow = Math.max(...b.seats.map(extractSeatNumber));
-      return { ...b._doc, maxRow };
+      const seatNumbers = b.seats.map(extractSeatNumber);
+      const refRow =
+        strategy === "max"
+          ? Math.max(...seatNumbers)
+          : Math.min(...seatNumbers);
+      return { ...b._doc, refRow };
     });
 
+    // sort logic
     processed.sort((a, b) => {
-      if (b.maxRow === a.maxRow) return a.booking_id - b.booking_id;
-      return b.maxRow - a.maxRow;
+      if (a.refRow === b.refRow) {
+        return a.booking_id - b.booking_id; // tie-breaker
+      }
+
+      return strategy === "max"
+        ? b.refRow - a.refRow // back to front
+        : a.refRow - b.refRow; // front to back
     });
 
     const sequence = processed.map((b, i) => ({
@@ -30,7 +44,11 @@ export const getBoardingSequence = async (req, res) => {
       seats: b.seats,
     }));
 
-    res.json(sequence);
+    res.json({
+      strategy,
+      totalBookings: sequence.length,
+      sequence,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
